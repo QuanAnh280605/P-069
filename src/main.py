029dfe0485f3f050e10a639d231bfeb78d5a7205
Thangph18 <hieuthangcr7@gmail.com>
2026-08-09 21:20:53 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -39,9 +39,6 @@ app.add_middleware(
 
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(router, prefix="/api/v1")
-app.include_router(auth_router)
-app.include_router(router)
-
 
 @app.get("/health")
 async def health():
